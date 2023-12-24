a4a202ca27243e04afc63531cfaf0650906cd41d
Gabry34 <gabrieleconciauro34@gmail.com>
2023-12-24 14:28:25 +0100

    commit

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -4,6 +4,7 @@ import RandomMovie from "@/components/RandomMovie";
 import UpcomingMovies from "@/components/sliders/UpcomingMovies";
 import TopRatedMovies from "@/components/sliders/TopRatedMovies";
 import Categories from "@/components/sliders/Categories";
+//bb
 
 export default function Home() {
   const apiKey = process.env.REACT_APP_API_KEY;
